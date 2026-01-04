8f06b2f7b1d1e59147be483e1e96fddbc9c08058
Tech2you <wpmpbz2srs@privaterelay.appleid.com>
2026-01-04 21:45:48 +0200

    feat: initialize Supabase env loading and app bootstrap

--- immortalink/lib/main.dart
+++ immortalink/lib/main.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
 import 'screens/create_vault_screen.dart';
 import 'screens/sign_in_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
